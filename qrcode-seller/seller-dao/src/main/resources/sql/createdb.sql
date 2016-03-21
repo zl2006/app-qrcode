638c753e1d6bcdb1d9af62ea30ca87abf19dc0cf
zl2006 <zl2006@gmail.com>
2016-03-21 14:30:52 +0800

    清理工程

--- qrcode-seller/seller-dao/src/main/resources/sql/createdb.sql
+++ qrcode-seller/seller-dao/src/main/resources/sql/createdb.sql
@@ -1,28 +1,28 @@
 --创建数据库和用户，以及授权操作
 
 --创建数据库
-create database if not exists market default charset utf8 collate utf8_general_ci;
+create database if not exists qrcode default charset utf8 collate utf8_general_ci;
 
 --创建用户
-CREATE USER market_admin  IDENTIFIED  BY 'market_admin';			--管理员
-CREATE USER market_app IDENTIFIED  BY 'market_app' ;						--应用程序使用
-CREATE USER market_main IDENTIFIED  BY 'market_main';					--维护员
+CREATE USER qrcode_admin  IDENTIFIED  	BY 'qrcode_admin';						--管理员
+CREATE USER qrcode_app IDENTIFIED  		BY 'qrcode_app' ;							--应用程序使用
+CREATE USER qrcode_main IDENTIFIED  	BY 'qrcode_main';							--维护员
 
 
 --授权, 安全原因实际环境中一定要指定ｉｐ地址，不能是％号。
-grant all privileges on market.* to market_admin@"%" identified  by 'market_admin';
-grant all privileges on market.* to market_admin@"127.0.0.1" identified  by 'market_admin';
-grant all privileges on market.* to market_admin@"localhost" identified  by 'market_admin';
+grant all privileges on qrcode.* to qrcode_admin@"%" identified  by 'qrcode_admin';
+grant all privileges on qrcode.* to qrcode_admin@"127.0.0.1" identified  by 'qrcode_admin';
+grant all privileges on qrcode.* to qrcode_admin@"localhost" identified  by 'qrcode_admin';
 flush privileges;
 
-grant  select, update, delete,insert  on market.* to market_app@"%" identified  by 'market_app';
-grant  select, update, delete,insert   on market.* to market_app@"127.0.0.1" identified  by 'market_app';
-grant  select, update, delete,insert   on market.* to market_app@"localhost" identified  by 'market_app';
+grant  select, update, delete,insert  on qrcode.* to qrcode_app@"%" identified  by 'qrcode_app';
+grant  select, update, delete,insert   on qrcode.* to qrcode_app@"127.0.0.1" identified  by 'qrcode_app';
+grant  select, update, delete,insert   on qrcode.* to qrcode_app@"localhost" identified  by 'qrcode_app';
 flush privileges;
 
-grant  select   on market.* to market_main@"%" identified  by 'market_main';
-grant  select   on market.* to market_main@"127.0.0.1" identified  by 'market_main';
-grant  select   on market.* to market_main@"localhost" identified  by 'market_main';
+grant  select   on qrcode.* to qrcode_main@"%" identified  by 'qrcode_main';
+grant  select   on qrcode.* to qrcode_main@"127.0.0.1" identified  by 'qrcode_main';
+grant  select   on qrcode.* to qrcode_main@"localhost" identified  by 'qrcode_main';
 flush privileges;
 
 

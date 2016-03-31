3f9ad06ed675b04004e88e79d10503779150373f
zl2006 <zl2006@gmail.com>
2016-03-31 17:27:44 +0800

    增加注解

--- qrcode-seller/seller-model/dto/src/main/java/org/yy/qrcodeseller/dto/product/ProductDto.java
+++ qrcode-seller/seller-model/dto/src/main/java/org/yy/qrcodeseller/dto/product/ProductDto.java
@@ -8,7 +8,11 @@
 */
 package org.yy.qrcodeseller.dto.product;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.yy.framework.basedata.query.AbstractQueryDto;
+import org.yy.framework.document.anno.Field;
+import org.yy.framework.document.anno.Model;
+import org.yy.framework.document.anno.TypeEnum;
 
 /**
 * 产品查询条件
@@ -17,27 +21,44 @@ import org.yy.framework.basedata.query.AbstractQueryDto;
 * @version  [1.0, 2016年3月28日]
 * @since  [qrcodeseller/1.0]
 */
+@Model(name = "产品查询条件", desc = "产品查询条件")
 public class ProductDto extends AbstractQueryDto {
     
     /**
     * 注释内容
     */
-    private static final long serialVersionUID = -8234931831830069376L;
+    private static final long serialVersionUID = 86725680222751630L;
     
     /**
      * 产品名称
      */
+    @Field(cnname = "产品名称", desc = "产品名称", type = TypeEnum.STRING, length = 128)
     private String name;
     
     /**
      * 产品编码
      */
+    @Field(cnname = "产品编码", desc = "产品编码，可以由产品名称的首个字母组成", type = TypeEnum.STRING, length = 64)
     private String prdCode;
     
     /**
      * 产品类目
      */
+    @Field(cnname = "产品标识", desc = "产品标识，数据库自动增长", type = TypeEnum.INTETER)
     private Integer categoryID;
+    
+    /**
+     * 产品状态
+     */
+    @Field(cnname = "状态", desc = "状态，0为无效，1为有效", type = TypeEnum.INTETER)
+    private Integer status;
+    
+    /**
+     * 产品所属商家
+     */
+    @Field(cnname = "卖家昵称", desc = "卖家昵称", type = TypeEnum.STRING, length = 128, nullable = false)
+    @NotEmpty(message = "卖家昵称不能为空！")
+    private String sellerNick;
     
     /**
     * @return 返回 name
@@ -79,6 +100,34 @@ public class ProductDto extends AbstractQueryDto {
     */
     public void setCategoryID(Integer categoryID) {
         this.categoryID = categoryID;
+    }
+    
+    /**
+    * @return 返回 status
+    */
+    public Integer getStatus() {
+        return status;
+    }
+    
+    /**
+    * @param 对status进行赋值
+    */
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+    
+    /**
+    * @return 返回 sellerNick
+    */
+    public String getSellerNick() {
+        return sellerNick;
+    }
+    
+    /**
+    * @param 对sellerNick进行赋值
+    */
+    public void setSellerNick(String sellerNick) {
+        this.sellerNick = sellerNick;
     }
     
 }
